6b2df7e9e1a14d0e820c7093ff90249ae7c532f4
oliver-tbs <113384797+oliver-tbs@users.noreply.github.com>
2024-10-17 10:03:33 -0400

    Reinstated ProB functionality

--- ApplyProBSettings.cs
+++ ApplyProBSettings.cs
@@ -1,66 +1,76 @@
-//using System;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Azure.WebJobs;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.Logging;
-//using Microsoft.Graph;
-//using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Newtonsoft.Json;
 
-//namespace appsvc_fnc_dev_scw_sensitivity_dotnet001
-//{
-//    public class ApplyProBSettings
-//    {
-//        [FunctionName("ApplyProBSettings")]
-//        public async Task RunAsync([QueueTrigger("prob", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
-//        {
-//            log.LogInformation($"ApplyProBSettings received a request: {myQueueItem}");
+namespace appsvc_fnc_dev_scw_sensitivity_dotnet001
+{
+    public class ApplyProBSettings
+    {
+        [FunctionName("ApplyProBSettings")]
+        public async Task RunAsync([QueueTrigger("prob", Connection = "AzureWebJobsStorage")] string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"ApplyProBSettings received a request: {myQueueItem}");
 
-//            dynamic data = JsonConvert.DeserializeObject(myQueueItem);
+            dynamic data = JsonConvert.DeserializeObject(myQueueItem);
 
-//            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).AddEnvironmentVariables().Build();
 
-//            string groupId = data?.groupId;
-//            string labelId = config["proBLabelId"];
-//            string DisplayName = data?.DisplayName;
-//            string requestId = data?.Id;
+            string groupId = data?.groupId;
+            string itemId = data?.itemId;
+            string labelId = config["proBLabelId"];
+            string ownerId = config["ownerId"]; // sv-caupdate@devgcx.ca
+            string requestId = data?.Id;
+            string spaceNameEn = data?.SpaceName;
+            string spaceNameFr = data?.SpaceNameFR;
 
-//            string itemId = data?.itemId;
+            ROPCConfidentialTokenCredential auth = new ROPCConfidentialTokenCredential(log);
+            var graphClient = new GraphServiceClient(auth);
 
-//            ROPCConfidentialTokenCredential auth = new ROPCConfidentialTokenCredential(log);
-//            var graphClient = new GraphServiceClient(auth);
+            var result = Common.ApplyLabel(graphClient, labelId, groupId, itemId, requestId, spaceNameEn, spaceNameFr, log);
+            
+            if (result.Result == true)
+            {
+                bool result1 = await SetProB(graphClient, groupId, log);
+                bool result2 = await Common.RemoveOwner(graphClient, groupId, ownerId, log);
 
-//            var result = Common.ApplyLabel(graphClient, labelId, groupId, itemId, requestId, DisplayName, log);
+                bool success = result1 && result2;
 
-//            if (result.Result == true)
-//            {
-//                await SetProB(graphClient, groupId, log);
-//                await Common.RemoveOwner(graphClient, groupId, "e4b36075-bb6a-4acf-badb-076b0c3d8d90", log);
-//                await Common.AddToEmailQueue(requestId, groupId, DisplayName, (string)data?.RequesterName, (string)data?.RequesterEmail, log);
-//            }
+                if (success)
+                {
+                    await Common.AddToStatusQueue(itemId, log);
+                    await Common.AddToEmailQueue(requestId, groupId, spaceNameEn, spaceNameFr, (string)data?.RequesterName, (string)data?.RequesterEmail, log);
+                }
+            }
 
-//            log.LogInformation($"ApplyProBSettings processed a request.");
-//        }
+            log.LogInformation($"ApplyProBSettings processed a request.");
+        }
 
-//        private static async Task<IActionResult> SetProB(GraphServiceClient graphClient, string groupId, ILogger log)
-//        {
-//            log.LogInformation("SetProB received a request.");
+        private static Task<bool> SetProB(GraphServiceClient graphClient, string groupId, ILogger log)
+        {
+            log.LogInformation("SetProB received a request.");
 
-//            try
-//            {
-//                var group = new Microsoft.Graph.Group { Visibility = "Private" };
-//                await graphClient.Groups[groupId].Request().UpdateAsync(group);
-//            }
-//            catch (Exception e)
-//            {
-//                log.LogError($"Message: {e.Message}");
-//                if (e.InnerException is not null) log.LogError($"InnerException: {e.InnerException.Message}");
-//                log.LogError($"StackTrace: {e.StackTrace}");
-//            }
+            bool result = true;
 
-//            log.LogInformation("SetProB processed a request.");
+            try
+            {
+                var group = new Microsoft.Graph.Group { Visibility = "Private" };
+                graphClient.Groups[groupId].Request().UpdateAsync(group);
+            }
+            catch (Exception e)
+            {
+                log.LogError($"Message: {e.Message}");
+                if (e.InnerException is not null) log.LogError($"InnerException: {e.InnerException.Message}");
+                log.LogError($"StackTrace: {e.StackTrace}");
+                result = false;
+            }
 
-//            return new OkResult();
-//        }
-//    }
-//}
+            log.LogInformation("SetProB processed a request.");
+
+            return Task.FromResult(result);
+        }
+    }
+}
